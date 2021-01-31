a6ee8bb8c20330507e15228485bef8b9dc6ec209
Aloysius Thomas <aloysiusthomas@xeoscript.com>
2021-01-31 20:08:36 +0530

    time table view

--- vadhyar/publicapp/utils.py
+++ vadhyar/publicapp/utils.py
@@ -1,4 +1,7 @@
+from publicapp.models import courses
 from publicapp.models import CustomUser
+from publicapp.models import subjects
+from publicapp.models import Teacher
 from publicapp.models import TimeTable
 
 
@@ -6,6 +9,16 @@ def generate_timetable():
     from datetime import datetime
     today = datetime.now().date()
     if not TimeTable.objects.filter(date=today).exists():
-        teachers = CustomUser.objects.filter(user_type__in=[2, 3])
+        teachers = CustomUser.objects.filter(user_type__in=[2, 4])
         for teacher in teachers:
-            TimeTable.objects.create(teacher=teacher, date=today, )  # todo: add time
+            teacher_profile = Teacher.objects.get(teacher=teacher)
+            session = TimeTable.objects.create(teacher=teacher, date=today, time=teacher_profile.available_time)
+            if teacher.user_type == 1:
+                course = subjects.objects.filter(teacher_id=teacher).first()
+                session.subject = course
+            else:
+                course = courses.objects.filter(trainee_id=teacher).first()
+                session.course = course
+            session.save()
+
+    return TimeTable.objects.filter(date=today)
